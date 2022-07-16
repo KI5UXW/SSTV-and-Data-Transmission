8801873c937515f8fbe5054f80263d45d8b4374f
James Ervin <ervinhfamily@gmail.com>
2022-07-16 10:27:34 -0500

    Bugfixes and Definition Creation

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
-# program to capture single image from webcam in python
+def takePic():
+    # program to capture single image from webcam in python
   
-# importing OpenCV library
-import cv2
+    # importing OpenCV library
+    import cv2
   
-# initialize the camera
-# If you have multiple camera connected with 
-# current device, assign a value in cam_port 
-# variable according to that
-cam_port = 0
-cam = cv2.VideoCapture(0)
+    # initialize the camera
+    # If you have multiple camera connected with 
+    # current device, assign a value in cam_port 
+    # variable according to that
+    cam_port = 0
+    cam = cv2.VideoCapture(0)
   
-# reading the input using the camera
-result, image = cam.read()
+    # reading the input using the camera
+    result, image = cam.read()
   
-# If image will detected without any error, 
-# show result
-if result:
+    # If image will detected without any error, 
+    # show result
+    if result:
   
-    # showing result, it take frame name and image 
-    # output
-    cv2.imshow("AMEA", image)
+        # showing result, it take frame name and image 
+        # output
+        cv2.imshow("AMEA", image)
   
-    # saving image in local storage
-    cv2.imwrite("AMEA.png", image)
+        # saving image in local storage
+        cv2.imwrite("AMEA.png", image)
   
-    # If keyboard interrupt occurs, destroy image 
-    # window
-    cv2.waitKey(0)
-    cv2.destroyWindow("GeeksForGeeks")
+        # If keyboard interrupt occurs, destroy image 
+        # window
+        cv2.waitKey(0)
+        cv2.destroyWindow("GeeksForGeeks")
   
-# If captured image is corrupted, moving to else part
-else:
-    print("No image detected. Please! try again")
+    # If captured image is corrupted, moving to else part
+    else:
+        print("No image detected. Please! try again")
