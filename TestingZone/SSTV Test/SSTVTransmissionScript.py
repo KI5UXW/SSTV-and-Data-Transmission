5a9a4b68d4424406a2c10861a5e49784ced92379
James Ervin <ervinhfamily@gmail.com>
2022-07-18 17:17:52 -0500

    Bugfix.

--- TestingZone/SSTV Test/SSTVTransmissionScript.py
+++ TestingZone/SSTV Test/SSTVTransmissionScript.py
@@ -4,6 +4,6 @@ from pysstv.color import Robot36
 baseImage = Image.open(r'C:\Users\hubba\Documents\GitHub\SSTV-and-Data-Transmission\TestingZone\SSTV Test\maxresdefault.jpg')
 baseImage = baseImage.resize((Robot36.WIDTH, Robot36.HEIGHT))
 sstv = Robot36(baseImage, 44100, 16)
-sstv.write_wav('NASASEES.wav')
+sstv.write_wav(r'C:\Users\hubba\Documents\GitHub\SSTV-and-Data-Transmission\TestingZone\SSTV Test\NASASEES.wav')
 filename = r'C:\Users\hubba\Documents\GitHub\SSTV-and-Data-Transmission\TestingZone\SSTV Test\NASASEES.wav'
 winsound.PlaySound(filename, winsound.SND_FILENAME)
